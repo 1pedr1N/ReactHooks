f46d98ef557bc9aea401fdc613e42ddd1e05cc3e
Pedro Silva <88340056+1pedr1N@users.noreply.github.com>
2023-03-08 16:17:45 -0300

    fix

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ export default function App() {
 const [location, setLocation] = useState({});
 useEffect(() => {
 
-navigator.geolocation.watchPosition(handlePositionReceived);
-
+const watchId = navigator.geolocation.watchPosition(handlePositionReceived);
+return () => navigator.geolocation.clearWatch(watchId);
 }, []);
 function handlePositionReceived({ coords }) {
   setLocation({
